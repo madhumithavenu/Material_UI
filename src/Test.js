49917b2686569c295362c9c340475e03938f0760
Madhumitha <madhumithavenu03@gmail.com>
2023-11-16 14:53:05 +0530

    material ui button component

--- src/Test.js
+++ src/Test.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {Typography} from '@mui/material';
+import {Typography, Button} from '@mui/material';
 
 const Test = () => {
   return (
     <div>
         <Typography variant='h5' sx={{color:'red', border: '1px solid black', borderRadius:10}}>Hello World</Typography>
+        <Button onClick={()=>alert("Button Clicked")} color='success' sx={{margin:3}} size='large' variant='contained'>First</Button>
+        <Button onClick={()=>alert("Button Clicked")} color='error' sx={{margin:3}} size='medium' variant='outlined'>Second</Button>
+        <Button onClick={()=>alert("Button Clicked")} color='info' sx={{margin:3}} size='small' variant='text'>Third</Button>
     </div>
   )
 }
